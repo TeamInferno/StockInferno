9077f8d068b8b923c1dc281a26d0ffc7e5474556
ryanlondon <ryel590@gmail.com>
2017-12-06 08:58:54 -0800

    Login-signup flow

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const userController = require('./userController')
+
 const app = express();
 // const router = express.Router();
 
@@ -11,8 +13,28 @@ app.use(express.static(path.join(__dirname + './../public')));
 app.use(express.static(path.join(__dirname + './../build')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + './../splash.html')));
+app.get('/signup', (req, res) => res.sendFile(path.join(__dirname + './../signup.html')));
 app.get('/home', (req, res) => res.sendFile(path.join(__dirname + './../home.html')));
 
+
+//** Login/Signup API Routes **************************************/
+app.post('api/login',
+  userController.getUser,
+  userController.validateUser,
+  (req, res) => {
+    console.log(req.body.username, req.body.password);
+    res.status(200).redirect('/home')
+  }
+);
+
+app.post('api/signup',
+  userController.createUser,
+  (req, res) => {
+    res.status(200).redirect('/home')
+  }
+);
+
+//** Stock API Routes *********************************************/
 app.get('/api/stocks/:symbol/:timePeriod', (req, res) => {
   stockAPIController.getData,
   console.log()
--- server/userController.js
+++ server/userController.js
@@ -1,33 +1,35 @@
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const db = require('./model');
 
 const SALT_WORK_FACTOR = 10;
 
 const userController = {}
 
-userController.getAllUsers = (req, res, next) => {
-  db.query('SELECT * FROM users')
-  .then((result) => {
-    console.log(result.rows);
-    db.end();
-    next();
-  })
-  .catch(err => console.error('Error getting all users', err.stack));
-};
-
 userController.getUser = (req, res, next) => {
   db.query('SELECT * FROM users WHERE username = $1', [req.body.username])
     .then(result => {
-      console.log(result.rows);
+      // Attach to returned user res.locals for validation in other middleware
+      res.locals.user = result.rows[0];
       db.end();
       next();
     })
     .catch(err => console.error('Error getting user', err.stack));
 };
 
+userController.validateUser = (req, res, next) => {
+  const user = res.locals.user;
+  if (!user || !(bcrypt.compareSync(req.user.password, user.password))) {
+    res.status(401).redirect('/signup');
+  } else {
+    res.locals.sessionId = user._id;
+    next();
+  }
+};
+
 userController.createUser = (req, res, next) => {
+  const password = bcrypt.hashSync(req.body.password, SALT_WORK_FACTOR);
   db.query('INSERT INTO users (name, username, password) VALUES ($1, $2, $3)',
-    [req.body.name, req.body.username, req.body.password])
+    [req.body.name, req.body.username, password])
     .then(result => {
       console.log(result);
       db.end();
@@ -36,17 +38,9 @@ userController.createUser = (req, res, next) => {
     .catch(err => console.error('Error creating user', err.stack));
 };
 
-userController.deleteUser = (req, res, next) => {
-  db.query('DELETE FROM users WHERE username = $1', [req.body.username])
-    .then()
-};
-
-// userController.getAllUsers();
-// userController.getUser({ body: { username: 'ryan' } }, null, () => {});
-// userController.createUser(
-//   { body: { name: 'Santa Claus', username: 'santa', password: 'corn' } }, 
-//   null, 
-//   () => {}
-// );
+// userController.deleteUser = (req, res, next) => {
+//   db.query('DELETE FROM users WHERE username = $1', [req.body.username])
+//     .then()
+// };
 
 module.exports = userController;
