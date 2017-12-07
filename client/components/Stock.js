4c6227f67dca1ca18b02e8caa1399b7858f98ddc
ryanlondon <ryel590@gmail.com>
2017-12-06 17:07:34 -0800

    added $query to graph route

--- client/components/Stock.js
+++ client/components/Stock.js
@@ -6,7 +6,7 @@ const Stock = (props) => {
   const arrow = props.diff >= 0 ? "stock green" : "stock red"
   return (
     <div className={arrow}>
-      <strong>{props.symbol}</strong> Change: {props.diff}    Open: {props.open}    Close: {props.close} <Link to={'/graph' + props.symbol}>Go To Graph</Link>  
+      <strong>{props.symbol}</strong> Change: {props.diff}    Open: {props.open}    Close: {props.close} <Link to={'/graph/$' + props.symbol}>Go To Graph</Link>  
     </div>
   );
 }
