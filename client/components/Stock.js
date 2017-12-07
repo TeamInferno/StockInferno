34217e5bde2e8f561fb63b0bde88a53d0683b978
ryanlondon <ryel590@gmail.com>
2017-12-06 17:03:55 -0800

    got adding stocks working

--- client/components/Stock.js
+++ client/components/Stock.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Stock = (props) => {
 
   const arrow = props.diff >= 0 ? "stock green" : "stock red"
   return (
     <div className={arrow}>
-      {props.symbol} {props.diff} {props.open} {props.close} 
+      <strong>{props.symbol}</strong> Change: {props.diff}    Open: {props.open}    Close: {props.close} <Link to={'/graph' + props.symbol}>Go To Graph</Link>  
     </div>
   );
 }
